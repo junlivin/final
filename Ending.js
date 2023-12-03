41d9409ea321bbaf26fa786b66502e9a6cdaa54d
junlivin <143903583+junlivin@users.noreply.github.com>
2023-12-02 22:29:29 -0500

    Add files via upload

---
+++ Ending.js
@@ -0,0 +1,37 @@
+
+  var ScreenTwoVid = document.getElementById("sceneSecond");
+  var ScreenOneVid = document.getElementById("sceneFirst");
+  let jumpToScreenTwo = document.getElementById('jumpToScreenTwo');
+  let jumpToScreenOne = document.getElementById('jumpToScreenOne');
+  let buttonOne = document.getElementById('playSceneOne')
+  let firstTime = true;
+
+  document.querySelector(".sceneTwoButtons").style.visibility = "visible";
+
+    (function (){
+      [].forEach.call(document.getElementsByClassName('sceneTwoButtons'), function(btn) {
+        btn.addEventListener('click', function(e) {
+
+          switch (e.target.id){
+          case "angry":
+            ScreenTwoVid.src = "video/mountainView.mp4";
+            ScreenTwoVid.play()
+              break;
+            case "smile":
+              ScreenTwoVid.src = "video/mountainView.mp4";
+              ScreenTwoVid.play()
+              break;
+            case "roll":
+              ScreenTwoVid.src = "video/cloudLadder.mp4";
+              ScreenTwoVid.play()
+              break;
+            case "indifferent":
+              ScreenTwoVid.src = "video/mountainView.mp4";
+              ScreenTwoVid.play()
+              break;
+            default:
+              }
+            })
+        })
+      }())
+
